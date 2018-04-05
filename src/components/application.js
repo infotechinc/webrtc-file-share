0bdaac2daa94ce93fb822c4d607dbc44094c113e
Kbarrera123 <kaylabarrera@ufl.edu>
2018-04-05 11:38:49 -0400

    Added final file upload component

--- src/components/application.js
+++ src/components/application.js
@@ -1,6 +1,7 @@
 import "./app-header.js";
 import "./landing-page.js";
 import "./loading-page.js";
+import "./file-upload-page.js";
 
 const template = `
 
@@ -12,58 +13,7 @@ const template = `
 
 <loading-page v-if="room_id && !peerIsReady"></loading-page>
 
-<!-- File upload page to show when peer is ready (Hides previous)-->
-<section class="section">
-  <div v-if="peerIsReady">
-    <div class="content has-text-centered">
-
-      <h2><br>Connection complete!<br><br></h2>
-      <p>Your friend has connected! Now choose the file you would like to transfer.<br><br></p>
-
-      <div class="file is-centered">
-        <!-- File Input Button -->
-        <label class="file-label">
-        <input class="file-input" type="file" name="resume" @change = "onFileChange"><br><br><br>
-        <span class="file-cta">
-          <span class="file-icon">
-            <i class="fas fa-upload"></i>
-          </span>
-          <span class="file-label">
-            Choose a file...
-          </span>
-        </span>
-      </label>
-    </div>
-
-  </div>
-
-  <div class="columns">
-    <div class="column is-half has-text-centered">
-      <!-- To make the column colored: notification is-primary -->
-     <h1><strong>Sent Files</strong></h1>
-      <!-- Dynamic progress bar -->
-      <div v-for="sentFile in sentFiles">
-        <div>
-          <!-- Show checkbox if file is fully sent -->
-          <p><span v-if="sentFile.complete" class="icon has-text-primary">
-            <i class="fas fa-check-square"></i>
-          </span>
-          {{ sentFile.name }}</p>
-          <progress v-if="!sentFile.complete" class="progress is-primary" v-bind:value="sentFile.bytesSent" v-bind:max="sentFile.size"></progress>
-        </div>
-      </div>
-    </div>
-    <div class="column is-half has-text-centered">
-      <h1><strong>Received Files</strong></h1>
-        <div v-for="receivedFile in receivedFiles">
-          <a v-bind:download=receivedFile.name v-bind:href=receivedFile.file>{{ receivedFile.name }}</a>
-        </div>
-    </div>
-  </div>
-
-
-  </div>
-</section>
+<file-upload-page v-if="peerIsReady" v-bind:sentFiles="sentFiles" v-bind:receivedFiles="receivedFiles" v-on:onFileChange="onFileChange"></file-upload-page>
 
 </div>
 `;
@@ -146,14 +96,12 @@ Vue.component("application", {
       });
       return peer;
     },
-    onFileChange: function(event) {
-      if (event.target.files.length == 0) return;
-      console.log(event);
-      const sender = this.peer.sendFile(event.target.files[0]);
+    onFileChange: function(fileData) {
+      const sender = this.peer.sendFile(fileData.file);
       this.sentFiles.unshift({
-        name: event.target.files[0].name,
+        name: fileData.file.name,
         bytesSent: 0,
-        size: event.target.files[0].size,
+        size: fileData.file.size,
         complete: false
       });
       sender.on("progress", bytesSent => {
